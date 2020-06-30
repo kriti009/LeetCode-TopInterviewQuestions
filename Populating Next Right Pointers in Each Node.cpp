88df5e459025436d0de4ea980a2cdfd58beaac6b
Kriti Dewangan <kriti.ritu.dew@gmail.com>
2020-06-30 09:16:34 +0530

    BFS

---
+++ Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root==NULL || root->left==NULL && root->right==NULL)
+            return root;
+        queue<Node*> q;
+        Node* temp;
+        q.push(root);
+        q.push(NULL);
+        while(!q.empty()){
+            temp = q.front();
+            q.pop();
+            if(temp==NULL){
+                if(q.empty()){
+                   break;
+                }
+                 q.push(NULL);
+            }else{
+                temp->next = q.front();
+                if(temp->left!=NULL)q.push(temp->left);
+                if(temp->right!=NULL)q.push(temp->right);
+            }
+        }
+        return root;
+    }
+};
